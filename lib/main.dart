4ed5097ef37e2de777960c9e69ba2a806193dda5
anabel <anabl.0812@gmail.com>
2020-09-10 00:07:09 -0500

    Calendario en español

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/routes/routes.dart';
 //import 'package:componentes/src/pages/home_temp.dart';
 import 'package:flutter/material.dart';
- 
+import 'package:flutter_localizations/flutter_localizations.dart'; 
+
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
@@ -11,6 +12,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''), // English, no country code
+        const Locale('es', 'ES'), 
+      ],
       initialRoute: '/',
       //home: HomePage(),
       routes: getAplicationRoutes(),
